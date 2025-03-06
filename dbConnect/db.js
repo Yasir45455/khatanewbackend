ad40375c24291a9988f5eeae5e32b246d4ab1ca0
Yasir45455 <149455805+Yasir45455@users.noreply.github.com>
2025-03-06 10:15:32 +0500

    Update db.js

--- dbConnect/db.js
+++ dbConnect/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://khataapp4:khataapp4@cluster0.y9jhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+mongoose.connect("mongodb+srv://khataapp4:khataapp4@cluster0.y9jhp.mongodb.net/KhaataApp?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
     console.log('dataBase Connected');
 });
 
